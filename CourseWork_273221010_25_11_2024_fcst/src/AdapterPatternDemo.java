762565f34754c27e99c1972c490c5d7c106b5c20
jerryrebuilds <jerryrebuilds01@gmail.com>
2024-11-25 14:31:18 +0200

    Code Upload For Course Work

---
+++ CourseWork_273221010_25_11_2024_fcst/src/AdapterPatternDemo.java
@@ -0,0 +1,10 @@
+public class AdapterPatternDemo {
+    public static void main(String[] args) {
+        AudioPlayer audioPlayer = new AudioPlayer();
+
+        audioPlayer.play("mp3", "song.mp3");
+        audioPlayer.play("mp4", "movie.mp4");
+        audioPlayer.play("vlc", "anime.vlc");
+        audioPlayer.play("avi", "video.avi");
+    }
+}
